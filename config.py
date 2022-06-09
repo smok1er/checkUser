7c2edd6574076e9d45cb381ecac70fc1e8aafd87
MohammedNajih <102456093+MohammedNajih@users.noreply.github.com>
2022-06-09 17:12:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "5354250888:AAF7dXCj8sgUyVpRkaZiH3y1MlZ1n9Kew3s"
-APP_URL = "https://checuser.herokuapp.com/" + BOT_TOKEN
+BOT_TOKEN = "5562243917:AAEvF5TturlE3azYckHPmFJWdwfkngwqElI"
+APP_URL = "https://facebockbot.herokuapp.com/" + BOT_TOKEN
